543bd3b0b042f71a8976b90e17af19b9a2497e80
zaszasxs <jessiejame7987@gmail.com>
2021-08-20 15:32:09 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program 
+    {
+        static void Main (string[] args)
+
+            //class
+            //Field (ข้อมูลที่ class สนใจ)
+            //constructure
+            //method
+            //properties get,set
+        {
+            //var s1 = new sub1()
+            //{
+            //    Name = "jason", Score = 91
+            //};
+
+
+            // obj เก็บข้อมูลแบบ value
+            ////s2 = s1; //  class เก็บข้อมูลแบบ ref (ร่วมกัน)                 
+            //var ใช้แทน datatype เพราะ ขึ้นอยู่กับค่าที่กำหนดเป็นอะไร
+
+            //var s2 = new sub1()
+            //{
+            //    Name = "joe", Score = 75
+
+            //};
+
+            //var b = new sub1[2] { s1, s2 };
+
+            //greadcal cal1 = new greadcal();
+            //cal1.cal(b); // 2 overload method cal
+
+            var a = new sub2()
+            {
+
+            };
+            a.S = 10000;
+            Console.WriteLine(a.S);
+            
+            
+
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/sub1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //public class sub1
+    //{
+    //    public int Score;
+    //    public string Name;
+
+    //    //public sub1(int score, string name) //constructure มีหน้าที่ กำหนดค่าก่อนสร้าง obj
+    //    //{
+    //    //    Score = score;
+    //    //    Name = name;
+
+    //    //    // ถ้าเราสร้าง constrator แล้ว จะไม่มีตัว default มาให้
+
+    //    //}
+
+    //    //public sub1()
+    //    //{
+    //    //    // แต่สร้างเองได้นะ
+    //    //}
+    //}
+
+    //public class greadcal
+    //{
+    //    public void cal(sub1[] b)
+    //    {
+    //        for (int i = 0; i < b.Length; i++)
+    //        {
+    //            var grade = cal(b[i]);
+    //            Console.WriteLine(grade);
+    //        }
+    //    }
+
+    //    public string cal(sub1 a)
+    //    {
+    //        if (a.Score > 90) return "A";
+    //        else if (a.Score > 80) return "B";
+    //        else if (a.Score > 70) return "C";
+    //        else if (a.Score > 60) return "D";
+    //        else return "F";
+
+    //    }
+
+       
+    //}
+
+    public  class sub2
+    {
+        public int s; // para ของ private มักใช้ตัวเล็ก
+
+        public int S
+        {
+            get // อ่านค่า
+            {
+                return s;
+            }
+            set // กำหนดค่า ทำเงื่อนไข
+            {
+                if (value > 100) s = 100;
+                else if (value < 100) s = 50;
+                else s = value;
+            }
+        }
+
+    }
+}
